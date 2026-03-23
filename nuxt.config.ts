94686c5d6d0e7f924871696d079cb93dd3c9eebf
Jose David yepes tumay <david@cloudnonic.com>
2026-03-22 23:47:37 -0500

    feat: add favicon and apple-touch-icon images with configuration updates

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,6 +6,10 @@ export default defineNuxtConfig({
     baseURL: '/',
     head: {
       link: [
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+        { rel: 'icon', type: 'image/png', href: '/favicon.png' },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
         { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
         { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },
         {
